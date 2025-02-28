9b51f9c679586d6c8ce38976be6c2b36a78f76cd
alx-calcio <alexandre.calciogaudino@gmail.com>
2025-02-28 16:26:17 +0100

    Deleted useless code

--- assets/js/script.js
+++ assets/js/script.js
@@ -52,7 +52,6 @@ addEventOnElements(navTogglers, "click", toggleNavbar);
  */
 
 const header = document.querySelector("[data-header]");
-const backTopBtn = document.querySelector("[data-back-top-btn]");
 
 let lastScrollPos = 0;
 
@@ -70,11 +69,9 @@ const hideHeader = function () {
 window.addEventListener("scroll", function () {
   if (window.scrollY >= 50) {
     header.classList.add("active");
-    backTopBtn.classList.add("active");
     hideHeader();
   } else {
     header.classList.remove("active");
-    backTopBtn.classList.remove("active");
   }
 });
 
